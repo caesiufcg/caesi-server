d235bc734a04ae696b486d9a951bead2287e70d0
victorhundo <victorhundo@gmail.com>
2018-09-07 12:30:49 -0300

    Testando incorporação do facebook

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
-from flask import Flask, request, abort
+from flask import Flask, request, render_template
 from flask_cors import CORS, cross_origin
 from flask_script import Manager
 import requests
 
 app = Flask(__name__)
-CORS(app)
+CORS(app) 
 
 @app.route("/")
 def hello():
-    return "EM BREVE!"
+     return render_template('hello.html')
